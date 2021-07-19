e91c6d9906c94bc98ff033651f8a2a6d6802afd5
Victor Nastase <victor.gabriel.nastase@gmail.com>
2021-07-19 10:18:59 +0300

    initial commit

---
+++ src/main/java/com/java/problems/problemsolver/ProblemSolverApplication.java
@@ -0,0 +1,13 @@
+package com.java.problems.problemsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/problems/problemsolver/ProblemSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.problems.problemsolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
